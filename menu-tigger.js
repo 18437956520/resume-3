554c299ccce2daf9bf107fdce506c2e40edcbc38
yyyh <444510796@qq.com>
2019-04-18 10:36:05 +0800

    js

---
+++ menu-tigger.js
@@ -0,0 +1,9 @@
+let liTags = document.getElementsByClassName('menuTigger')
+for (let i = 0; i < liTags.length; i++) {
+    liTags[i].onmouseenter = function (x) {
+        x.currentTarget.classList.add('active')
+    }
+    liTags[i].onmouseleave = function (x) {
+        x.currentTarget.classList.remove('active')
+    }
+}
