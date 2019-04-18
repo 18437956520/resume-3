417c55a192b2f917ebc709c2640855884a0f865a
yyyh <444510796@qq.com>
2019-04-18 14:27:21 +0800

    js

--- init-swiper.js
+++ init-swiper.js
@@ -3,19 +3,7 @@
   var controller = {
     view: null,
     swiper: null,
-    swiperOptions: {
-      // 循环模式选项
-      loop: true,
-      // 如果需要分页器
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      // 如果需要前进后退按钮
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-    },
+    swiperOptions: { loop: true, pagination: { el: '.swiper-pagination', }, navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', } },
     init: function (view) {
       this.view = view
       this.initSwiper()
