ff10b59790c2644062b34a4a9713c87aa112122e
yyyh <444510796@qq.com>
2019-04-18 14:26:38 +0800

    js

--- init-swiper.js
+++ init-swiper.js
@@ -2,19 +2,29 @@
   var view = document.querySelector('#mySwiper')
   var controller = {
     view: null,
+    swiper: null,
+    swiperOptions: {
+      // 循环模式选项
+      loop: true,
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    },
     init: function (view) {
-      var mySwiper = new Swiper(view.querySelector('.swiper-container'), {
-        loop: true, // 循环模式选项
-        // 如果需要分页器
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        // 如果需要前进后退按钮
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      })
+      this.view = view
+      this.initSwiper()
+    },
+    initSwiper: function () {
+      this.swiper = new Swiper(
+        view.querySelector('.swiper-container'),
+        this.swiperOptions
+      )
     }
   }
   controller.init(view)
